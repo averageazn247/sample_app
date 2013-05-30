6359c9bcee82138a97f321a89d44becb943b3cf7
khoa bui <averageazn1337@gmail.com>
2013-05-30 14:27:41 -0500

    deleted old migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130529214322) do
+ActiveRecord::Schema.define(:version => 20130530185238) do
+
+  create_table "events", :force => true do |t|
+    t.string   "user"
+    t.string   "name"
+    t.text     "description"
+    t.date     "dayof"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
