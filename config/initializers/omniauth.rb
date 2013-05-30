6656e69307ef76ee39589567984e1054da5db8f2
khoa bui <averageazn1337@gmail.com>
2013-05-30 13:33:07 -0500

    deleted more

--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["845586527578.apps.googleusercontent.com"], ENV["J7S60im3ff1s_MIoyY_jtivv"]
-             {
-             :scope => "userinfo.email,userinfo.profile,plus.me,http://gdata.youtube.com",
-             :approval_prompt => "auto"
-           }
-            
-end
